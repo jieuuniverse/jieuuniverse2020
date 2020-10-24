d2bb72876779daf2962ef019b48faa617f2a7f68
jieuuniverse <31970739+jieuuniverse@users.noreply.github.com>
2020-10-24 11:47:02 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+<!--START JS SCRIPTS-->
+
+$(document).ready(function(){ 
+  $(".projects").addClass("animated slideInUp");
+  $(".intro").addClass("animated slideInUp");
+  $(".grid").addClass("animated slideInUp");
+  $(".about").addClass("animated slideInUp");
+});
+<!--END JS SCRIPTS-->
